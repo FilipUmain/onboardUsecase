dc5b7c77ba8cfaff0f229fe56de9287480ef885e
Filip Karlström <filip.karlstrom@umain.com>
2024-05-30 13:55:44 +0200

    style fix

--- src/app/[Restaurant]/page.tsx
+++ src/app/[Restaurant]/page.tsx
@@ -1,6 +1,7 @@
 import MaxWidthWrapper from "@/components/MaxWidthWrapper";
 import { IRestaurant } from "@/models/IRestaurant";
 import { getRestaurant } from "@/utils/getRestaurant";
+import Image from "next/image";
 import React from "react";
 
 interface RestaurantProps {
@@ -17,6 +18,12 @@ const Restaurant = async ({ searchParams }: RestaurantProps) => {
       <MaxWidthWrapper>
         <section className="w-full p-6 h-[90vh] border rounded-[8px] bg-white">
           <h1>{restaurant.name}</h1>
+          <Image
+            src={restaurant.image_url}
+            height={100}
+            width={100}
+            alt="restaurant logo"
+          />
         </section>
       </MaxWidthWrapper>
     </section>
--- src/components/FilterSideBar.tsx
+++ src/components/FilterSideBar.tsx
@@ -1,10 +1,7 @@
 import React, { Dispatch, SetStateAction } from "react";
-import FilterContainer from "./FilterContainer";
 import DeliveryTime from "./DeliveryTime";
 import { IFilter } from "@/models/IFilter";
-import { getPriceRange } from "@/utils/getPriceRange";
 import { IRestaurant } from "@/models/IRestaurant";
-import { IPriceRange } from "@/models/IPriceRange";
 
 interface IFilterSideBarProps {
   filters: IFilter[];
@@ -19,9 +16,7 @@ interface IFilterSideBarProps {
 
 const FilterSideBar = ({
   filters,
-  restaurants,
   onFilterClick,
-  selectedFilter,
   selectedDeliveryTime,
   setSelectedDeliveryTime,
 }: IFilterSideBarProps) => {
@@ -29,25 +24,35 @@ const FilterSideBar = ({
     <div className="flex flex-col gap-3">
       <h2 className="text-2xl">Filter</h2>
       <h3 className="text-gray-400 my-2 font-semibold">FOOD CATEGORY</h3>
-      {filters.map((filter) => (
-        <span
-          className="p-3 border rounded-[8px] hover:bg-gray-100"
-          onClick={() => onFilterClick(filter)}
-          key={filter.id}
-        >
-          {filter.name}
-        </span>
-      ))}
+      <div className="flex flex-col gap-2">
+        {filters.map((filter) => (
+          <span
+            className="inline-block p-3 border rounded-[8px] hover:bg-gray-100 cursor-pointer w-max"
+            onClick={() => onFilterClick(filter)}
+            key={filter.id}
+          >
+            {filter.name}
+          </span>
+        ))}
+      </div>
       <DeliveryTime
         selectedDeliveryTime={selectedDeliveryTime}
         setSelectedDeliveryTime={setSelectedDeliveryTime}
       />
       <h3 className="text-gray-400 my-2 font-semibold">PRICE RANGE</h3>
-      <div className="flex gap-3">
-        <span className="p-3 border rounded-[8px] hover:bg-gray-200">$</span>
-        <span className="p-3 border rounded-[8px] hover:bg-gray-200">$$</span>
-        <span className="p-3 border rounded-[8px] hover:bg-gray-200">$$$</span>
-        <span className="p-3 border rounded-[8px] hover:bg-gray-200">$$$$</span>
+      <div className="flex flex-col gap-2">
+        <span className="inline-block px-3 py-2 border rounded-[8px] hover:bg-gray-200 cursor-pointer w-max">
+          $
+        </span>
+        <span className="inline-block px-3 py-2 border rounded-[8px] hover:bg-gray-200 cursor-pointer w-max">
+          $$
+        </span>
+        <span className="inline-block px-3 py-2border rounded-[8px] hover:bg-gray-200 cursor-pointer w-max">
+          $$$
+        </span>
+        <span className="inline-block px-3 py-2 border rounded-[8px] hover:bg-gray-200 cursor-pointer w-max">
+          $$$$
+        </span>
       </div>
     </div>
   );
